37be789de42e4c5d2c8c9f7577a920901f284550
Esra <esra.ashoor.e@gmail.com>
2024-02-27 16:38:27 +0300

    workinf in the event

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
 import Nav from './components/Nav'
 import Events from './components/Events'
 import Login from './components/Login'
 import Register from './components/Register'
+import eventsArray from './data/events'
 const App = () => {
+  const [events, setEvents] = useState(eventsArray)
+
   return (
     <div className="App">
       <header>{<Nav />}</header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="events" element={<Events />} />
+          <Route path="events" element={<Events events={events} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
