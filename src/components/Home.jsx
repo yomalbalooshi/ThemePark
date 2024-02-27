9048b437466c923069e5b6caa9bd33cf9812642e
Yasmeen <yomalbalooshi@gmail.com>
2024-02-27 14:10:21 +0300

    added home image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import Zone from './Zone'
 const Home = () => {
   const themeParkData = [
@@ -18,9 +19,45 @@ const Home = () => {
   ]
   return (
     <div>
-      <h1>homepage</h1>
-      {/* SVG FILE IMPORTING LINE */}
-      <img src="/images/1.svg" alt="try image" width="1200px" height="400px" />
+      <div className="zonemap-main-div">
+        <div className="zonemap-image-div">
+          <div className="zonemap-imagecontainer-div">
+            <img src="/images/zonesMap.svg" alt="try image" />
+          </div>
+        </div>
+        <div className="zonemap-information-div">
+          <h2 className="zonemap-information-title">Explore Our Zones</h2>
+          <p className="zonemap-information-paragraph">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in
+          </p>
+          <ul className="homepage-zone-ul">
+            <li className="homepage-zone-li">
+              <NavLink to="zone/waterZone" className="zone1">
+                Water Zone
+              </NavLink>
+            </li>
+            <li className="homepage-zone-li">
+              <NavLink to="zone/kidsZone" className="zone2">
+                Kids Zone
+              </NavLink>
+            </li>
+            <li className="homepage-zone-li">
+              <NavLink to="zone/castleZone" className="zone3">
+                Castle Zone
+              </NavLink>
+            </li>
+            <li className="homepage-zone-li">
+              <NavLink to="zone/beachZone" className="zone4">
+                Beach Zone
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
       {themeParkData.map((themePark) => (
         <Zone key={themePark.mapName} {...themePark} />
       ))}
